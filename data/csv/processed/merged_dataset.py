a7d8df86f68652b1cf4cf9035ec12b28c15edc07
Ramil Mammadov <ramil.mammadov2021@gmail.com>
2024-12-04 14:32:53 -0500

    updated the dataset

--- data/csv/processed/merged_dataset.py
+++ data/csv/processed/merged_dataset.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df1 = pd.read_csv('https://raw.githubusercontent.com/Cavidan-oss/IDS_706_Final_Project/refs/heads/main/data/csv/processed/city_facts.csv')
+df1 = pd.read_csv('https://raw.githubusercontent.com/Cavidan-oss/IDS_706_Final_Project/refs/heads/main/data/csv/raw/city_facts.csv')
 df2 = pd.read_csv('https://raw.githubusercontent.com/Cavidan-oss/IDS_706_Final_Project/refs/heads/main/data/csv/processed/uscities_processed.csv')
 
 merged_df = pd.merge(df1, df2, on='city')
