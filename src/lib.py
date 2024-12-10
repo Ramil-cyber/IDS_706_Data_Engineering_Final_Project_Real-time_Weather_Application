ef67a1ec67ad72320af2cb712c5cae45a24fc046
Cavidan-oss <cavidankarimli1@gmail.com>
2024-12-09 23:09:57 -0500

    Update the number of locations

--- src/lib.py
+++ src/lib.py
@@ -14,7 +14,10 @@ def process_raw_cities_csv(
 
 
 if __name__ == "__main__":
+    city_list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'San Francisco', 'Indianapolis', 'Columbus', 'Fort Worth', 'Charlotte', 'Seattle', 'Denver', 'El Paso', 'Detroit', 'Washington', 'Boston', 'Memphis', 'Nashville', 'Portland', 'Oklahoma City', 'Las Vegas', 'Baltimore', 'Louisville', 'Milwaukee', 'Kansas City', 'Cleveland', 'Tampa', 'Raleigh', 'Minneapolis', 'St. Louis', 'Miami', 'Salt Lake City', 'Tucson', 'Anchorage', 'Fresno', 'Birmingham', 'New Orleans', 'St. Petersburg', 'Macon', 'Lubbock', 'Shreveport', 'Peoria', 'Grand Rapids']
+
     process_raw_cities_csv(
         file_path="data/csv/raw/uscities.csv",
         target_file_path="data/csv/processed/uscities_processed.csv",
+        city_list=city_list
     )
